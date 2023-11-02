57c346b5ba9665377158858425049df2fd78e875
AmandaMatiello <almatiello@gmail.com>
2023-11-02 20:35:23 -0300

    Adicionei um comentário explicativo

--- Média-de-notas.py
+++ Média-de-notas.py
@@ -1,3 +1,4 @@
+#calcular a média das notas de um aluno e exibir uma mensagem na tela dizendo se ele foi aprovado (média >= 6) ou ficou de recuperação (média < 6)
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2)/2
